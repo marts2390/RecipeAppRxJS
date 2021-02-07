969bcb36791c602921993a1be41a46643695c310
Chris Martyr <chris@MacBook-Pro.local>
2021-02-07 16:48:57 +0000

    remove unused code

--- types/declaration.d.ts
+++ types/declaration.d.ts
@@ -1,18 +1,3 @@
-/*
- * (c) Copyright 2020 Preservica
- *  All rights reserved.
- *
- *  Developed By:
- *  Preservica
- *  32 The Quadrant
- *  Abingdon Science Park
- *  Abingdon
- *  OX14 3YS
- *
- *  email: info@preservica.com
- *  web: www.preservica.com
- */
-
 declare module '*.less' {
   const content: { [className: string]: string }
 
